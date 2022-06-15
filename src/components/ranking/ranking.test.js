7f5a0752b1394887ce253946d1932779cf68663a
damien-owl <the.grey.hibou@gmail.com>
2022-06-16 00:36:38 +0800

    new page for MSBL rankings

---
+++ src/components/ranking/ranking.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {act, render, fireEvent, cleanup, waitFor} from '@testing-library/react';
+
+import axiosMock from "axios";
+import Ranking from "./ranking";
+
+afterEach(cleanup);
+
+test('A list of MSBL Players, ordered by ranking, is displayed', async () => {
+    const gametype = 3;
+    const url = `https://api.mariostrikers.gg/ratings?gametype=${gametype}`;
+    axiosMock.get.mockResolvedValue({
+        data: [{
+                "rating": "1518",
+                "name": "Agent A",
+                "rank": "legend",
+                "numWins": 24,
+                "numLosses": 5,
+                "numDraws": 0
+            },
+            {
+                "rating": "976",
+                "name": "Jimray3",
+                "rank": "professional",
+                "numWins": 16,
+                "numLosses": 24,
+                "numDraws": 0
+            },
+            {
+                "rating": "1202",
+                "name": "xAlex",
+                "rank": "superstar",
+                "numWins": 16,
+                "numLosses": 13,
+                "numDraws": 0
+            }
+        ]
+    });
+
+    const {getByText, getByTestId, rerender} = render(<Ranking gametype={gametype}/>);
+
+    expect(getByText(/Rankings for/i).textContent).toBe("Rankings for Mario Strikers: Battle League")
+
+    const resolvedPlayer01Elem = await waitFor(() => getByTestId("player-name-0"));
+    const resolvedPlayer02Elem = await waitFor(() => getByTestId("player-name-1"));
+    const resolvedPlayer03Elem = await waitFor(() => getByTestId("player-name-2"));
+
+    expect((resolvedPlayer01Elem).textContent).toBe("Agent A");
+    expect((resolvedPlayer02Elem).textContent).toBe("xAlex");
+    expect((resolvedPlayer03Elem).textContent).toBe("Jimray3");
+
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(url);
+});
