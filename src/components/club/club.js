a8c3cdad33d08cb15e4ca8918c5b199b1ef7665a
damien-owl <the.grey.hibou@gmail.com>
2022-07-05 22:37:33 +0800

    club page incomplete draft

--- src/components/club/club.js
+++ src/components/club/club.js
@@ -1,13 +1,14 @@
 import "./club.css";
 
 import axios from "axios";
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useLayoutEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import no_img from "../../assets/no-image-icon.png";
+import LinkDiscordServer from "../links/discordserver";
 
 const Club = () => {
-    let { clubid } = useParams();
-    const [club, setClub] = useState(null);
+    let {clubid} = useParams();
+    const [club, setClub] = useState({});
     const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
@@ -22,6 +23,10 @@ const Club = () => {
             })
     }, []);
 
+    useLayoutEffect(() => {
+        window.scrollTo(0, 0)
+    });
+
     return (
         <div>
             <div className="columns">
@@ -33,20 +38,7 @@ const Club = () => {
                 </div>
                 <div className="column is-2"/>
             </div>
-            <div className="columns">
-                <div className="column is-2"/>
-                <div className="column is-2">
-                    <figure className="image is-128x128">
-                        {buildTeamImgElement(club.logo)}
-                    </figure>
-                    {buildClubJoinConditions(club.joinConditions)}
-                </div>
-                <div className="column is-6">
-                    <span className="tag">{club.name} [{club.tag}]</span>
-                    bla bla
-                </div>
-                <div className="column is-2"/>
-            </div>
+            {buildClubProfile(club)}
             <div className="columns">
                 <div className="column is-2"/>
                 <div className="column is-8">
@@ -56,19 +48,41 @@ const Club = () => {
                 </div>
                 <div className="column is-2"/>
             </div>
-            <div className="columns">
-                <div className="column is-2"/>
-                <div className="column is-6">
-                    <span className="tag">{club.roster[0].name} [{club.roster[0].country}]</span>
-                    bla bla
-                </div>
-                <div className="column is-2"/>
-            </div>
+
+            {buildClubRoster(isLoading, club)}
+
         </div>
 
     );
-
 };
+
+function buildClubProfile(club) {
+    return (
+        <div className="columns">
+            <div className="column is-2"/>
+            <div className="column is-2">
+                <figure className="image is-128x128">
+                    {buildTeamImgElement(club.logo)}
+                </figure>
+                {buildClubJoinConditions(club.joinConditions)}
+            </div>
+            <div className="column is-6">
+                <section className="hero is-small is-link">
+                    <div className="hero-body">
+                        <p className="title">
+                            {club.name}
+                        </p>
+                        <p className="subtitle">
+                            {club.tag} <br />
+                            {club.region} <br />
+                            {club.discordServer}
+                        </p>
+                    </div>
+                </section>
+            </div>
+            <div className="column is-2"/>
+        </div>);
+}
 
 function buildTeamImgElement(logo) {
     if (!logo) {
@@ -81,10 +95,34 @@ function buildTeamImgElement(logo) {
 }
 
 function buildClubJoinConditions(joinConditions) {
-    if (joinConditions && joinConditions.trim().toLowerCase() === "Open to Anyone".toLowerCase()) {
+    if (!joinConditions) {
+        return <div/>;
+    } else if (joinConditions.trim().toLowerCase() === "Open to Anyone".toLowerCase()) {
         return <span className="tag is-success">{joinConditions}</span>;
     } else {
         return <span className="tag is-warning">{joinConditions}</span>;
+    }
+}
+
+function buildClubRoster(isLoading, club) {
+    if (!isLoading && club && club.roster && club.roster.length > 0) {
+        return (
+            <div className="columns">
+                <div className="column is-2"/>
+                <div className="column is-6" id="placeholder-table-while-club-is-loading">
+                    <span className="tag">{club.roster[0].name} {club.roster[0].country}</span>
+                </div>
+                <div className="column is-2"/>
+            </div>
+        );
+    } else {
+        return (
+            <div className="columns">
+                <div className="column is-2"/>
+                <div className="column is-6" id="placeholder-table-while-club-is-loading"/>
+                <div className="column is-2"/>
+            </div>
+        );
     }
 }
 
